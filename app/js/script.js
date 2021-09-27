cb5fdc3eafa00af82a810844a46422b35c8cafcb
DavidGit999 <itprog999@gmail.com>
2021-09-27 21:00:05 +0300

    Add Calculator class

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,36 @@
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    this.clear()
+  }
+  
+  clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operaion = undefined
+  }
+  
+  delete() {
+    
+  }
+  appendNumber(number) {
+
+  }
+
+  chooseOperaion(operaion) {
+
+  }
+
+  compute() {
+    
+  }
+
+  updateDisplay() {
+
+  }
+}
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
